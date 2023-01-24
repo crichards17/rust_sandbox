d4f2357b08edc3d5b14afa758d586f19eb30d5fe
cameron_richards <crichards1717@gmail.com>
2023-01-23 21:10:54 -0800

    search refactored with iterator methods

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -47,15 +47,20 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+    //  ----- ^^Unnecessary intermediate mutable state (results)
 
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
